ce6e036075995e56970f71d2012ab2ae7742f9c5
Raziur306 <raziur.rahaman.ronju@gmail.com>
2023-03-21 22:48:54 +0600

    statusbar & navigation color

--- app/src/main/java/com/example/notes/presentation/theme/Color.kt
+++ app/src/main/java/com/example/notes/presentation/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.notes.ui.theme
+package com.example.notes.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/notes/presentation/theme/Type.kt
+++ app/src/main/java/com/example/notes/presentation/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.notes.ui.theme
+package com.example.notes.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
