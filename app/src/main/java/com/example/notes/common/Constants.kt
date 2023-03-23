1bbb04bb6a6163ac434b37b8d7388cf0975039c2
Raziur306 <raziur.rahaman.ronju@gmail.com>
2023-03-23 12:22:06 +0600

    error comp & Constants value

--- app/src/main/java/com/example/notes/common/Constants.kt
+++ app/src/main/java/com/example/notes/common/Constants.kt
@@ -2,4 +2,7 @@ package com.example.notes.common
 
 object Constants {
     const val BASE_URL = "https://notes-api-azure.vercel.app"
+    const val NOTE_BODY_PARAM = "noteBody"
+    const val NOTE_TITLE_PARAM = "noteTitle"
+    const val NOTE_ID_PARAM = "noteId"
 }
---
+++ app/src/main/java/com/example/notes/presentation/common/component/AuthError.kt
@@ -0,0 +1,31 @@
+package com.example.notes.presentation.common.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.notes.R
+
+@Composable
+fun AuthError(message: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(
+            space = 10.dp,
+        ),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(painter = painterResource(id = R.drawable.error), contentDescription = "Error Icon")
+        Text(text = message, fontSize = 14.sp)
+    }
+}
