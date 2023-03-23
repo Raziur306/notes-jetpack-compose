8d15311f9a59f08a286fda7993a5211ac7893782
Raziur306 <raziur.rahaman.ronju@gmail.com>
2023-03-23 12:25:27 +0600

    progress compose

---
+++ app/src/main/java/com/example/notes/presentation/common/component/LoadingProgress.kt
@@ -0,0 +1,20 @@
+package com.example.notes.presentation.screens.sign_in.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoadingProgress() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
